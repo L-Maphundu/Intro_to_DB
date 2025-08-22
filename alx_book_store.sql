902cb853cd7cb4f791f685e099f9c911756e55c9
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-22 20:21:02 +0200

    testing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,34 +2,34 @@ CREATE DATABASE alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    author_id PRIMARY KEY,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Books (
-    book_id PRIMARY KEY,
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    author_id, --FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE
 );
 
 CREATE TABLE Customers (
-    customer_id PRIMARY KEY,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
-    address TEXT,
+    address TEXT
 );
 
 CREATE TABLE Orders (
-    order_id PRIMARY KEY,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_id INT PRIMARY KEY,
+    cutomer_id, --FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
-    orderdetailid PRIMARY KEY,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    orderdetailid INT PRIMARY KEY,
+    order_id, --FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id, --FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
