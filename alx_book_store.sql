4175876376619e4f0508e0ada2bfb22f0138af26
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-22 20:00:03 +0200

    testing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,3 +21,15 @@ CREATE TABLE Customers (
     address TEXT,
 );
 
+CREATE TABLE Orders (
+    order_id PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
