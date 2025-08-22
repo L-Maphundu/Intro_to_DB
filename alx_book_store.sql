c73974f9aa31b6b84963bc563c6559b29b89fc14
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-08-22 20:27:24 +0200

    testing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -9,7 +9,8 @@ CREATE TABLE Authors (
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id, --FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
     publication_date DATE
 );
@@ -23,13 +24,16 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
-    cutomer_id, --FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
-    order_id, --FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    book_id, --FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
